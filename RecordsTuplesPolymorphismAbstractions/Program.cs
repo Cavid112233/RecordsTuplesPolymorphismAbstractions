a55cd5499f95e540de6665b72ca3cb87c353f4cb
Cavid112233 <suleymanli535@gmail.com>
2024-04-05 23:35:41 +0400

    Add project files.

---
+++ RecordsTuplesPolymorphismAbstractions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordsTuplesPolymorphismAbstractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
