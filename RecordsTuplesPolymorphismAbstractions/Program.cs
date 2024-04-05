6b3c3d23bc0ebbdb3c495eb85064b8f1be659621
Cavid112233 <suleymanli535@gmail.com>
2024-04-06 00:51:12 +0400

    Finished

--- RecordsTuplesPolymorphismAbstractions/Program.cs
+++ RecordsTuplesPolymorphismAbstractions/Program.cs
@@ -8,60 +8,109 @@ namespace RecordsTuplesPolymorphismAbstractions
         {
             Blog blog = new Blog();
 
-
             int choice;
             do
             {
-                Console.WriteLine("1.Blog yarat");
-                Console.WriteLine("0.Proqrami bitir!");
-                if (!int.TryParse(Console.ReadLine(), out choice))
+                Console.WriteLine("1. Blog elave et:");
+                Console.WriteLine("0. Programi bitir!");
+
+                if (int.TryParse(Console.ReadLine(), out choice))
                 {
                     switch (choice)
                     {
                         case 1:
-                            Console.WriteLine("Blog basligi:");
+                            Console.Write("Blog basligi:");
                             string title = Console.ReadLine();
-                            Console.WriteLine("Blog aciglamasi:");
+                            Console.Write("Blog aciglamasi:");
                             string description = Console.ReadLine();
 
-                            if(Helper.CheckBlogTitle(title) && Helper.CheckBlogDescription(description))
+                            if (Helper.CheckBlogTitle(title) && Helper.CheckBlogDescription(description))
                             {
                                 blog = new Blog(title, description);
-                            }
-                            int inChoice;
-                            do
-                            {
-                                Console.WriteLine("1.Comment yarat");
-                                Console.WriteLine("2. Commentlere bax");
-                                Console.WriteLine("3. Commente bax");
-                                Console.WriteLine("4. Commenti sil");
-                                Console.WriteLine("5. Commenti guncele");
-                                Console.WriteLine("0.Menyuya qayit");
-                                if (int.TryParse(Console.ReadLine(), out inChoice))
-                                {
-                                    switch (inChoice)
-                                    {
-                                        case 1:
-                                            Console.Write("Commentin contenti: ");
-                                            string content = Console.ReadLine();
-                                            Comment comment = new Comment(content);
-                                            blog.AddComment(comment);
-                                            Console.WriteLine("Comment elave olundu!");
-                                            break;
-                                        case 2:
-                                            Console.WriteLine("Butun commentlere bax:");
-                                            foreach (var a in blog.GetAllComments())
-                                            {
-                                                Console.WriteLine(a);
-                                            }
-                                            break;
-                                    }
-                            } while (true);
+                                Console.WriteLine("Blog yaradildi!");
 
+                                int inChoice;
+                                do
+                                {
+                                    Console.WriteLine("1. Comment yarat:");
+                                    Console.WriteLine("2. Commentlere bax:");
+                                    Console.WriteLine("3. Commente bax:");
+                                    Console.WriteLine("4. Commenti sil:");
+                                    Console.WriteLine("5. Commenti guncellesdir:");
+                                    Console.WriteLine("0. Menyuya qayit:");
+
+                                    if (int.TryParse(Console.ReadLine(), out inChoice))
+                                    {
+                                        switch (inChoice)
+                                        {
+                                            case 1:
+                                                Console.Write("Commentin mezmunu: ");
+                                                string content = Console.ReadLine();
+                                                Comment comment = new Comment(content);
+                                                blog.AddComment(comment);
+                                                Console.WriteLine("Comment elave edildi!");
+                                                break;
+                                            case 2:
+                                                Console.WriteLine("Butun commentler:");
+                                                foreach (var c in blog.GetAllComments())
+                                                {
+                                                    Console.WriteLine(c);
+                                                }
+                                                break;
+                                            case 3:
+                                                Console.Write("Comment-in id-si: ");
+                                                int commentId = int.Parse(Console.ReadLine());
+                                                var selectedComment = blog.GetComment(commentId);
+                                                if (selectedComment != null)
+                                                    Console.WriteLine(selectedComment);
+                                                else
+                                                    Console.WriteLine("Comment tapilmadi!");
+                                                break;
+                                            case 4:
+                                                Console.Write("Commentin idsi: ");
+                                                int removeId = int.Parse(Console.ReadLine());
+                                                blog.RemoveComment(removeId);
+                                                Console.WriteLine("Comment silindi!");
+                                                break;
+                                            case 5:
+                                                Console.Write("Commentin idsi: ");
+                                                int updateId = int.Parse(Console.ReadLine());
+                                                Console.Write("Yeni comment movzusu: ");
+                                                string newContent = Console.ReadLine();
+                                                Comment updatedComment = new Comment(newContent);
+                                                blog.UpdateComment(updateId, updatedComment);
+                                                Console.WriteLine("Comment yenilendi!");
+                                                break;
+                                            case 0:
+                                                break;
+                                            default:
+                                                Console.WriteLine("Duzgun secim edilmeyib!");
+                                                break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Düzgün seçim edilmədi.");
+                                    }
+                                } while (inChoice != 0);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Baslig 5-den boyuk olmalidir ve aciglama 8-den boyuk olmalidir!");
+                            }
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            Console.WriteLine("Sehv secim edildi!");
                             break;
                     }
                 }
-            } while (true);
+                else
+                {
+                    Console.WriteLine("Sehv secim edilib!");
+                }
+            } while (choice != 0);
         }
     }
 }
